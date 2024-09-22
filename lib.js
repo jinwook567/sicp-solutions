82d3a1ea848caf62e0973238a550b1defe9363e0
jinwook567 <70192587+jinwook567@users.noreply.github.com>
2024-09-23 01:48:02 +0900

    common functions

---
+++ lib.js
@@ -0,0 +1,41 @@
+export function pair(a, b) {
+  function dispatch(type) {
+    return type === 0 ? a : type === 1 ? b : "pair";
+  }
+  return dispatch;
+}
+
+export function is_pair(pair) {
+  return typeof pair === "function" && pair(2) === "pair";
+}
+
+export function head(pair) {
+  return pair(0);
+}
+
+export function tail(pair) {
+  return pair(1);
+}
+
+export function is_null(list) {
+  return list === null;
+}
+
+export function map(f, items) {
+  return is_null(items) ? null : pair(f(head(items)), map(f, tail(items)));
+}
+
+export function accumulate(op, initial, items) {
+  return is_null(items)
+    ? initial
+    : accumulate(op, op(initial, head(items)), tail(items));
+}
+
+export function append(list1, list2) {
+  return is_null(list1) ? list2 : pair(head(list1), append(tail(list1), list2));
+}
+
+export function list(...args) {
+  const [first, ...rest] = args;
+  return args.length === 0 ? null : pair(first, list(rest));
+}
