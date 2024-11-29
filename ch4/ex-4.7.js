d70833ccf9a442ba0b010187c73a8ee971a4b14f
jinwook567 <70192587+jinwook567@users.noreply.github.com>
2024-11-29 20:41:36 +0900

    solution 4.7

---
+++ ch4/ex-4.7.js
@@ -0,0 +1,34 @@
+import { is_tagged_list } from "./sec-4.1.2.js";
+import { tail, head } from "../lib.js";
+import { make_application } from "./sec-4.1.2.js";
+
+// a
+function is_while_loop(component) {
+  return is_tagged_list(component, "while_loop");
+}
+
+function while_predicate(component) {
+  return head(tail(component));
+}
+
+function while_block(component) {
+  return head(tail(tail(component)));
+}
+
+// b
+function while_loop(predicate, job) {
+  function iter() {
+    if (predicate()) {
+      job();
+      iter();
+    }
+  }
+  iter();
+}
+
+// c
+function while_to_application(component) {
+  const predicate = while_predicate(component);
+  const block = while_block(component);
+  return make_application(while_loop, list(predicate, block));
+}
