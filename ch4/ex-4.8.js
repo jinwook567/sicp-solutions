22426513bf18773affa18538d46d5d58000f721e
jinwook567 <70192587+jinwook567@users.noreply.github.com>
2025-01-15 18:34:10 +0900

    solution 4.8

---
+++ ch4/ex-4.8.js
@@ -0,0 +1,24 @@
+// a
+
+/**
+ * 3
+ * undefined
+ * 1
+ * 5
+ */
+
+// b
+function eval_sequence(stmts, env) {
+  if (is_empty_sequence(stmts)) {
+    return undefined;
+  } else if (is_last_statement(stmts)) {
+    return evaluate(first_statement(stmts), env);
+  } else {
+    const first_stmt_value = evaluate(first_statement(stmts), env);
+    if (is_return_value(first_stmt_value)) {
+      return first_stmt_value;
+    } else {
+      return eval_sequence(rest_statements(stmts), env) ?? first_stmt_value;
+    }
+  }
+}
